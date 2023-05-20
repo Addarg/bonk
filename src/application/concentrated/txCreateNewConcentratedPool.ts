1e645136773ab3708036c9e32c3ae1962de973f8
sanidhyajain1 <sanidhyajain077@gmail.com>
2023-05-20 15:38:14 +0530

    switch RPC successfull

---
+++ src/application/concentrated/txCreateNewConcentratedPool.ts
@@ -0,0 +1,21 @@
+import assert from '@/functions/assert'
+import txHandler from '../txTools/handleTx'
+import { generateCreateClmmPositionTx } from './txCreateConcentratedPosition'
+import useConcentrated from './useConcentrated'
+
+export default function txCreateNewConcentratedPool() {
+  return txHandler(async ({ transactionCollector }) => {
+    const { tempDataCache } = useConcentrated.getState()
+    assert(tempDataCache, 'should create pool first')
+    const createPoolInnerTransaction = tempDataCache
+    const { innerTransactions: openPositionInnerTransaction } = await generateCreateClmmPositionTx()
+
+    transactionCollector.add(createPoolInnerTransaction, {
+      txHistoryInfo: { title: 'Create pool', description: `create clmm pool` }
+    })
+
+    transactionCollector.add(openPositionInnerTransaction, {
+      txHistoryInfo: { title: 'Open pool position', description: `Open clmm pool position` }
+    })
+  })
+}
---
+++ src/application/pools/usePoolSummeryInfoLoader.tsx
@@ -0,0 +1,36 @@
+import jFetch from '@/functions/dom/jFetch'
+
+import useInit from '@/hooks/useInit'
+import useUpdate from '@/hooks/useUpdate'
+import useAppAdvancedSettings from '../common/useAppAdvancedSettings'
+import { usePools } from './usePools'
+
+type InfoResponse = {
+  tvl: string | number
+  totalvolume: string | number
+  volume24h: string | number
+}
+
+/** load tvl and volumn24h */
+export default function usePoolSummeryInfoLoader() {
+  const refreshCount = usePools((s) => s.refreshCount)
+  const tvl = usePools((s) => s.tvl)
+  const volume24h = usePools((s) => s.volume24h)
+  const infoUrl = useAppAdvancedSettings((s) => s.apiUrls.info)
+
+  const fetchSummeryInfo = async () => {
+    const summeryInfo = await jFetch<InfoResponse>(infoUrl)
+    if (!summeryInfo) return
+    usePools.setState({ tvl: summeryInfo.tvl, volume24h: summeryInfo.volume24h })
+  }
+
+  useInit(() => {
+    if (!tvl || !volume24h) {
+      fetchSummeryInfo()
+    }
+  })
+
+  useUpdate(() => {
+    fetchSummeryInfo()
+  }, [refreshCount, infoUrl])
+}
